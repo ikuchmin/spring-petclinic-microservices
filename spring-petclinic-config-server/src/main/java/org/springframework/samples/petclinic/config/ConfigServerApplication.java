85ce961982fa6b1c5a582bfd0ae3e5d94d5b09e0
ikuchmin <ikuchmin@gmail.com>
2025-04-10 20:25:27 +0400

    Implement full case with TimeWeb Deployment

--- spring-petclinic-config-server/src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
+++ spring-petclinic-config-server/src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.io.File;
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
+        String classpath = System.getProperty("java.class.path");
+        String[] classPathValues = classpath.split(File.pathSeparator);
+        System.out.println(Arrays.toString(classPathValues));
+
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 
