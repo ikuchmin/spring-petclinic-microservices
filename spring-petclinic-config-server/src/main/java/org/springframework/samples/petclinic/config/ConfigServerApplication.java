83e664170df133591f40cf1dfded21f5bf16850c
ikuchmin <ikuchmin@gmail.com>
2025-04-14 14:50:28 +0400

    Add not imported preconfigured config server

---
+++ spring-petclinic-config-server/src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
